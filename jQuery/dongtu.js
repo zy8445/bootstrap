67b881b2812a92bbcd719b3db5fbe4c47a02e42b
zhangying <675461534@qq.com>
2019-03-28 01:36:28 +0800

    第二章再完善

--- jQuery/dongtu.js
+++ jQuery/dongtu.js
@@ -1,6 +1,11 @@
-$(function(){
-	$("#iconfont").mouseenter(function(){
-		$(this).animate({font-size:"30px"});
-		console.log(111);
-	})
-})
+//$(function(){
+//	$("#iconfont").mouseenter(function(){
+//		$(this).animate({font-size:"30px"});
+//		console.log(111);
+//	})
+//})
+//$(function(){
+//	$(".shuzi a").mouseenter(function(){
+//		$(".shuziLeft").animate({"transform":"rotateY(360deg)","background":"white","color":"#af0000","transition":"0.7"})
+//	})
+//})
